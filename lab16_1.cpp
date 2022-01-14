355495de4f3335a3225baeb972d000b8aa13c4eb
Nanashikung <nata.station@hotmail.com>
2022-01-14 15:31:22 +0700

    lab16_1.cpp complete case 2

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main(){
 	int a=5;
-	string b="A";
-	string &c=b;
+	char b='A';
+	char &c=b;
 	int *x=&a;
-	string *y=&b;
+	char *y=&b;
 	int *z=x;
-	cout << a << " " << b << " "<< c << " "<< x << " "<< y << " "<< z <<endl;
-	cout << &a << " " << &b << " "<< &c << " "<< &x << " "<< &y << " "<< &z <<endl;
+	cout << a << " " << b << " "<< c << " "<< x << " "<< (int *)y << " "<< z <<endl;
+	cout << &a << " " << (int *)&b << " "<< (int *)&c << " "<< &x << " "<< &y << " "<< &z <<endl;
 	c = 'F';
-	cout << a << " " << b << " "<< c << " "<< x << " "<< y << " "<< z <<endl;
+	cout << a << " " << b << " "<< c << " "<< x << " "<< (int *)y << " "<< z <<endl;
 	*y = 'W';
-	cout << a << " " << b << " "<< c << " "<< x << " "<< y << " "<< z <<endl;
+	cout << a << " " << b << " "<< c << " "<< x << " "<< (int *)y << " "<< z <<endl;
 	*x = 6;
-	cout << a << " " << b << " "<< c << " "<< x << " "<< y << " "<< z <<endl;
+	cout << a << " " << b << " "<< c << " "<< x << " "<< (int *)y << " "<< z <<endl;
 	*z = 7;
-	cout << a << " " << b << " "<< c << " "<< x << " "<< y << " "<< z <<endl;
+	cout << a << " " << b << " "<< c << " "<< x << " "<< (int *)y << " "<< z <<endl;
 	return 0;
 }
